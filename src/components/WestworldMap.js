e9287fa0ea1b5b0f45ae5a3664b17890395a6fc6
Blair <blairacarroll@gmail.com>
2019-04-23 12:50:45 -0400

    complete

--- src/components/WestworldMap.js
+++ src/components/WestworldMap.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import Area from './Area.js';
 import { Segment } from 'semantic-ui-react';
 
 
-const WestworldMap = () => {
+const WestworldMap = ({ areas, hosts, selectedHostId, selectHost }) => {
+
+  const renderHosts = (value, limit) => (hosts.filter(host => host.area === value))
+
+  const renderAreas = () => {
+    return areas.map(({ namesObject: { text, value }, formatName, limit }) =>
+      <Area
+        value={value}
+        name={text}
+        limit={limit}
+        key={value}
+        hosts={renderHosts(value, limit)}
+        selectedHostId={selectedHostId}
+        selectHost={selectHost}
+      />
+    )
+  }
 
   return (
     <Segment id="map" >
-      {/* What should we render on the map? */}
+      {renderAreas()}
     </Segment>
   )
 }
---
+++ src/services/Adapter.js
@@ -0,0 +1,21 @@
+const URL = "http://localhost:4000/"
+
+export class Adapter {
+
+    static fetchHosts() {
+        return fetch(URL + "hosts")
+            .then(res => res.json())
+    }
+
+    static fetchAreas() {
+        return fetch(URL + "areas")
+            .then(res => res.json())
+    }
+
+}
+
+
+import { Adapter } from '/services/Adapter';
+
+Adapter.fetchHosts()
+    .then(res => th)
